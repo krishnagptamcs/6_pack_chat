7b51c1619267527e134d27ca7f9656e6d82dcd71
Krishna Gupta <121439479+krishnagptamcs@users.noreply.github.com>
2024-03-01 00:18:28 +0530

    socket connect to frontend

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import {CssBaseline} from "@mui/material";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    {/* applying the basic css */}
+    <CssBaseline />
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
--- server/app.js
+++ server/app.js
@@ -5,8 +5,14 @@ import { createServer } from "http";
 const PORT = 3000;
 const app = express();
 
-const server = createServer(app);// attaching socket io to http server
-const io = new Server(server); // circuit
+const server = createServer(app); // attaching socket io to http server
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true, // inside header credentials allow true
+  },
+}); // circuit
 
 io.on("connection", (socket) => {
   console.log("user is connected ");
@@ -17,6 +23,6 @@ app.get("/", (req, res) => {
   res.send("welcome to krishn a server");
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log("server is running on ", PORT);
 });
